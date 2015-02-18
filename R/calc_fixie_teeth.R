37b9db20100acc007be47e024f75c61696cbb487
jannes-m <malnamalja@gmx.de>
2015-02-18 20:35:05 +0100

    calc_fixie_teeth: updating help file and adding function argument tol

--- R/calc_fixie_teeth.R
+++ R/calc_fixie_teeth.R
@@ -1,23 +1,31 @@
 #' @title Front and rear gear teeth (Fixie).
 #' @description \code{calc_fixie_teeth} calculates the optimal number of front
-#' and rear gear teeth for a given gear ratio/transmission.
+#' and rear gear teeth for a given gear ratio. 
 #' @param ratio The desired gear ratio given as a positive numeral.
+#' @param tol Tolerance around which the specified gear ratio might deviate with
+#'   0.1 as default.
 #' @param nrow An integer indicating the maximal number of rows which should be
 #' returned by the function.
 #' @details The function assumes that the chainring (front sprocket) varies in
-#'   size from 20 teeth to 59 and the cog varies from 14 teeth to 21 teeth.
+#'   size from 31 teeth to 59 and the cog varies from 14 teeth to 21 teeth.
 #' @return The function returns a dataframe with following columns:
 #' \enumerate{
-#' \item{\strong{front} The optimal number of chainring teeth.}
-#' \item{\strong{rear} The optimal number of cog teeth.}
-#' \item{\strong{ratio} The gear ratio.}
+#' \item{\strong{front} The optimal number of chainring teeth in accordance with
+#' the number of skid patches and the specified \code{ratio} (see below for more
+#' details).}
+#' \item{\strong{rear} The optimal number of cog teethi n accordance with the
+#' number of skid patches and the specified \code{ratio} (see below for more 
+#' details).}
+#' \item{\strong{ratio} The actual gear ratio.}
 #' \item{\strong{skid_1} The number of skid patches for single-legged skidders.
 #' See \code{\link{calc_skids}} for more details.}
 #' \item{\strong{skid_2} The number of skid patches for two-legged skidders.
 #' See \code{\link{calc_skids}} for more details.}
 #' }
-#' The function displays all possible combinations in descending order which
-#' range +/- 0.1 around the desired gear ratio.
+#' Internally, the function uses a tolerance of 0.1 around the specified 
+#' \code{ratio} for its calculations. Additionally, it sorts the results first
+#' by \code{skid_2} in descending, and second by \code{ratio} in ascending
+#' order.
 #' @author Jannes Muenchow
 #' @importFrom dplyr mutate_ mutate filter_ arrange select
 #' @importFrom lazyeval interp
@@ -25,22 +33,20 @@
 #' @keywords Single speed bike, Fixie
 #' @examples
 #' calc_fixie_teeth(2.8, 5)
-calc_fixie_teeth <- function(ratio = 2.8, nrow = 5) {
-  if (ratio %% 1 == 0 | ratio <= 2) {
-    snrow(paste0("Function argument ratio must be positive and",
+calc_fixie_teeth <- function(ratio = 2.8, tol = 0.1, nrow = 5) {
+  if (ratio %% 1 == 0 | ratio <= 1) {
+    stop(paste0("Function argument ratio must be positive and",
                 " must not be an integer!"))
   }
   # range of the number of teeth of the front sprocket
   front <- 31:59
   # range of the number of teeth of the rear sprocket
   rear <- 14:21
-  # define the tolerance
-  tol <- 0.1
   # expand
   d <- expand.grid("front" = front, "rear" = rear)
   # find the transmission ratio for each combination
   d <- mutate(d, ratio = front / rear)
-  # build the condition
+  # define the condition
   cond <- interp(~ ratio >= min & ratio <= max,
                  min = ratio - tol, max = ratio + tol)
   # subset according to the condition
